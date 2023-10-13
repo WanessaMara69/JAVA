59e0bebe00d68048b1671984c56e7082207ffe43
WanessaMara <wanessamaralima15@gmail.com>
2023-10-13 11:49:17 -0300

    Create OperadoresAritmeticos.java

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,54 @@
+package com.wanessa.cursojava.aula13;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		
+		int resultado = 1 + 2;
+		System.out.println(resultado);
+		
+		resultado = resultado - 1;
+		System.out.println(resultado);
+	
+		resultado = resultado * 2;
+		System.out.println(resultado);
+		
+		resultado = resultado / 2;
+		System.out.println(resultado);
+		
+		resultado = resultado + 8;
+		System.out.println(resultado);
+		
+		resultado = resultado % 7; // % qualquer resultado que divida por 7, eu quero o valor do resto 
+		System.out.println(resultado);
+			
+		String primeiroNome = " Esta é ";
+		String segundoNome = "uma String concatenada. ";
+		String terceiroNome = primeiroNome + segundoNome;
+		System.out.println(terceiroNome);
+		
+		resultado = resultado + 1;
+		System.out.println(resultado);
+		
+		resultado++;
+		System.out.println(resultado);
+		
+		//5
+		System.out.println(resultado++);
+		//mesma coisa que
+		//System.out.println(resultado);		
+		//resultado = resultado + 1;
+		//resultado += 1;  // linha 37,40,41 são a mesma coisa
+		
+		System.out.println(++resultado);
+		//mesma coisa que 
+		//resultado += 1;
+		//System.out.println(resultado);
+		
+		resultado--;
+		System.out.println(resultado);
+		
+		System.out.println(resultado--);
+		System.out.println(--resultado);
+	}
+}
